75ea761d895d05be3d30472be6fed2cbeed6a50c
dennohkesh <dennismacharianyaga@gmail.com>
2022-11-11 14:59:40 +0300

    first commits

---
+++ index.php
@@ -0,0 +1,105 @@
+
+<?php
+error_reporting(0);
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Blood Donation Management System</title>
+	
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!--// Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="registration/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<!-- //Web-Fonts -->
+
+</head>
+
+<body>
+	<?php
+	include('includes/header.php');
+	?>
+	
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+
+	<!-- banner -->
+	<div class="slider">
+		<div class="callbacks_container">
+			<ul class="rslides callbacks callbacks1" id="slider4">
+				<li>
+					<div class="banner-top1">
+						<div class="banner-info_agile_w3ls">
+							<div class="container">
+								<h3>Blood Donation Services that you
+									<span>can Trust</span>
+								</h3>
+								
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="banner-top2">
+						<div class="banner-info_agile_w3ls">
+							<div class="container">
+								<h3>One Blood Donation Save three Lives
+									<span>every day</span>
+								</h3>
+								<p>Sometimes money cannot save life
+									<span>but donated blood can</span></p>
+						
+							</div>
+						</div>
+					</div>
+				</li>
+		
+			</ul>
+		</div>
+	</div>
+	<!-- //banner -->
+	<div class="clearfix"></div>
+
+	<?php include('includes/footer.php');?>
+
+</body>
+
+</html>
